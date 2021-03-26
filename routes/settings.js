c855734f9f1b8b0a7fb61358421315b4267aebfd
Gireg <gireg.dekerdanet@hotmail.fr>
2021-03-26 22:47:04 +0100

    add settings model

---
+++ routes/settings.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const { PermissionMiddlewareCreator } = require("forest-express-sequelize");
+const { settings } = require("../models");
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator("settings");
+
+// This file contains the logic of every route in Forest Admin for the collection settings:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Setting
+router.post("/settings", permissionMiddlewareCreator.create(), (request, response, next) => {
+  throw new Error("unauthorized");
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Setting
+router.put("/settings/:recordId", permissionMiddlewareCreator.update(), (request, response, next) => {
+  if (request.body.data.attributes.key) throw new Error("Vous ne pouvez pas modifier le nom du paramètre.");
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Setting
+router.delete("/settings/:recordId", permissionMiddlewareCreator.delete(), (request, response, next) => {
+  throw new Error("Unauthorized");
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Settings
+router.get("/settings", permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Settings
+router.get("/settings/count", permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Setting
+router.get("/settings/:recordId(?!count)", permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Settings
+router.get("/settings.csv", permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Settings
+router.delete("/settings", permissionMiddlewareCreator.delete(), (request, response, next) => {
+  throw new Error("Unauthorized");
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
