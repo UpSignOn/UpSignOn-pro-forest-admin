8772472c6a02956476f8093bd8f72eb91f89f7e2
Gireg <gireg.dekerdanet@hotmail.fr>
2021-03-27 18:50:00 +0100

    fix port

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-require('dotenv').config();
-const debug = require('debug')('{name}:server');
-const http = require('http');
-const chalk = require('chalk');
-const app = require('./app');
+require("dotenv").config();
+const debug = require("debug")("{name}:server");
+const http = require("http");
+const chalk = require("chalk");
+const app = require("./app");
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
 
-  if (Number.isNaN(port)) { return val; }
-  if (port >= 0) { return port; }
+  if (Number.isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
 
   return false;
 }
 
-const port = normalizePort(process.env.PORT || process.env.APPLICATION_PORT || '3310');
-app.set('port', port);
+const port = normalizePort(process.env.APPLICATION_PORT || "3310");
+app.set("port", port);
 
 const server = http.createServer(app);
 server.listen(port);
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? `Pipe ${port}`
-    : `Port ${port}`;
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
 
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -44,13 +46,11 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
   debug(`Listening on ${bind}`);
 
   console.log(chalk.cyan(`Your application is listening on ${bind}.`));
 }
 
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
