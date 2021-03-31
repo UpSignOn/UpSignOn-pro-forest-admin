1a209e611ff828316928ee99bec97940ef1e5619
Gireg <gireg.dekerdanet@hotmail.fr>
2021-03-31 19:49:06 +0200

    add stats to models

--- routes/data-stats.js
+++ routes/data-stats.js
@@ -1,61 +1,61 @@
 const express = require("express");
 const { PermissionMiddlewareCreator } = require("forest-express-sequelize");
-const { migrations } = require("../models");
+const { dataStats } = require("../models");
 
 const router = express.Router();
-const permissionMiddlewareCreator = new PermissionMiddlewareCreator("migrations");
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator("dataStats");
 
-// This file contains the logic of every route in Forest Admin for the collection migrations:
+// This file contains the logic of every route in Forest Admin for the collection dataStats:
 // - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
 // - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
 
-// Create a Migration
-router.post("/migrations", permissionMiddlewareCreator.create(), (request, response, next) => {
+// Create a Data Stat
+router.post("/dataStats", permissionMiddlewareCreator.create(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
   next();
 });
 
-// Update a Migration
-router.put("/migrations/:recordId", permissionMiddlewareCreator.update(), (request, response, next) => {
+// Update a Data Stat
+router.put("/dataStats/:recordId", permissionMiddlewareCreator.update(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
   next();
 });
 
-// Delete a Migration
-router.delete("/migrations/:recordId", permissionMiddlewareCreator.delete(), (request, response, next) => {
+// Delete a Data Stat
+router.delete("/dataStats/:recordId", permissionMiddlewareCreator.delete(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
   next();
 });
 
-// Get a list of Migrations
-router.get("/migrations", permissionMiddlewareCreator.list(), (request, response, next) => {
+// Get a list of Data Stats
+router.get("/dataStats", permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
   next();
 });
 
-// Get a number of Migrations
-router.get("/migrations/count", permissionMiddlewareCreator.list(), (request, response, next) => {
+// Get a number of Data Stats
+router.get("/dataStats/count", permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
   next();
 });
 
-// Get a Migration
-router.get("/migrations/:recordId(?!count)", permissionMiddlewareCreator.details(), (request, response, next) => {
+// Get a Data Stat
+router.get("/dataStats/:recordId(?!count)", permissionMiddlewareCreator.details(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
   next();
 });
 
-// Export a list of Migrations
-router.get("/migrations.csv", permissionMiddlewareCreator.export(), (request, response, next) => {
+// Export a list of Data Stats
+router.get("/dataStats.csv", permissionMiddlewareCreator.export(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
   next();
 });
 
-// Delete a list of Migrations
-router.delete("/migrations", permissionMiddlewareCreator.delete(), (request, response, next) => {
+// Delete a list of Data Stats
+router.delete("/dataStats", permissionMiddlewareCreator.delete(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
   next();
--- routes/usage-logs.js
+++ routes/usage-logs.js
@@ -1,61 +1,61 @@
 const express = require("express");
 const { PermissionMiddlewareCreator } = require("forest-express-sequelize");
-const { migrations } = require("../models");
+const { usageLogs } = require("../models");
 
 const router = express.Router();
-const permissionMiddlewareCreator = new PermissionMiddlewareCreator("migrations");
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator("usageLogs");
 
-// This file contains the logic of every route in Forest Admin for the collection migrations:
+// This file contains the logic of every route in Forest Admin for the collection usageLogs:
 // - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
 // - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
 
-// Create a Migration
-router.post("/migrations", permissionMiddlewareCreator.create(), (request, response, next) => {
+// Create a Usage Log
+router.post("/usageLogs", permissionMiddlewareCreator.create(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
   next();
 });
 
-// Update a Migration
-router.put("/migrations/:recordId", permissionMiddlewareCreator.update(), (request, response, next) => {
+// Update a Usage Log
+router.put("/usageLogs/:recordId", permissionMiddlewareCreator.update(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
   next();
 });
 
-// Delete a Migration
-router.delete("/migrations/:recordId", permissionMiddlewareCreator.delete(), (request, response, next) => {
+// Delete a Usage Log
+router.delete("/usageLogs/:recordId", permissionMiddlewareCreator.delete(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
   next();
 });
 
-// Get a list of Migrations
-router.get("/migrations", permissionMiddlewareCreator.list(), (request, response, next) => {
+// Get a list of Usage Logs
+router.get("/usageLogs", permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
   next();
 });
 
-// Get a number of Migrations
-router.get("/migrations/count", permissionMiddlewareCreator.list(), (request, response, next) => {
+// Get a number of Usage Logs
+router.get("/usageLogs/count", permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
   next();
 });
 
-// Get a Migration
-router.get("/migrations/:recordId(?!count)", permissionMiddlewareCreator.details(), (request, response, next) => {
+// Get a Usage Log
+router.get("/usageLogs/:recordId(?!count)", permissionMiddlewareCreator.details(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
   next();
 });
 
-// Export a list of Migrations
-router.get("/migrations.csv", permissionMiddlewareCreator.export(), (request, response, next) => {
+// Export a list of Usage Logs
+router.get("/usageLogs.csv", permissionMiddlewareCreator.export(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
   next();
 });
 
-// Delete a list of Migrations
-router.delete("/migrations", permissionMiddlewareCreator.delete(), (request, response, next) => {
+// Delete a list of Usage Logs
+router.delete("/usageLogs", permissionMiddlewareCreator.delete(), (request, response, next) => {
   throw new Error();
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
   next();
