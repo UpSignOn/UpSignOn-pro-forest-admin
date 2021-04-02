3b38631ccf1403659784324ecb89b1ab2b9a7eef
Gireg <gireg.dekerdanet@hotmail.fr>
2021-04-02 12:50:26 +0200

    fix stats

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,6 +6,42 @@ const Liana = require("forest-express-sequelize");
 const moment = require("moment");
 const models = require("../models");
 const Sequelize = require("sequelize");
+const { values } = require("../config/databases");
+
+const fillForEmptyDates = (values, options) => {
+  const result = [];
+  const orderedValues = values
+    .map((v) => ({ ...v, label: moment(v.label).startOf("day") }))
+    .sort((a, b) => (a.label.isBefore(b.label) ? -1 : 1));
+  const maxDate = moment().endOf("day");
+  let nextDate = orderedValues[0].label;
+  let lastKnownValueIndex = 0;
+  while (nextDate.isBefore(maxDate)) {
+    const lastKnownValue = orderedValues[lastKnownValueIndex] || orderedValues[orderedValues.length - 1];
+    const nextKnownValue = orderedValues[lastKnownValueIndex + 1] || orderedValues[orderedValues.length - 1];
+    console.log(lastKnownValue.label.format(), nextDate.format());
+    if (nextDate.isSame(lastKnownValue.label)) {
+      result.push({ label: nextDate.format(), values: { value: lastKnownValue.values.value } });
+    } else if (nextDate.isSame(nextKnownValue.label)) {
+      result.push({ label: nextDate.format(), values: { value: nextKnownValue.values.value } });
+      lastKnownValueIndex++;
+    } else {
+      if (options.with === "PREVIOUS_VALUE") {
+        result.push({
+          label: nextDate.format(),
+          values: { value: lastKnownValue.values.value },
+        });
+      } else {
+        result.push({
+          label: nextDate.format(),
+          values: { value: 0 },
+        });
+      }
+    }
+    nextDate = nextDate.clone().add(1, "day");
+  }
+  return result;
+};
 
 /* Cumulated nb of registered users */
 router.post("/stats/cumulated-nb-users", async (req, res) => {
@@ -23,7 +59,7 @@ router.post("/stats/cumulated-nb-users", async (req, res) => {
     return { label: r.day, values: { value: acc } };
   });
   let json = new Liana.StatSerializer({
-    value: values,
+    value: fillForEmptyDates(values, { with: "PREVIOUS_VALUE" }),
   }).perform();
   res.send(json);
 });
@@ -42,7 +78,25 @@ router.post("/stats/nb-active-users-per-day", async (req, res) => {
   const queryResult = await models.connections.default.query(query, { type: "SELECT" });
   const values = queryResult.map((r) => ({ label: r.day, values: { value: r.count } }));
   let json = new Liana.StatSerializer({
-    value: values,
+    value: fillForEmptyDates(values, { with: "0" }),
+  }).perform();
+  res.send(json);
+});
+
+/* Total nb sessions per day */
+router.post("/stats/total-nb-sessions-per-day", async (req, res) => {
+  const query = `SELECT
+  COUNT(logs.*) AS count,
+  date_trunc('day',logs.date) AS day
+  FROM usage_logs AS logs
+  WHERE AGE(logs.date) < interval '15 days'
+  GROUP BY day
+  ORDER BY day
+  `;
+  const queryResult = await models.connections.default.query(query, { type: "SELECT" });
+  const values = queryResult.map((r) => ({ label: r.day, values: { value: r.count } }));
+  let json = new Liana.StatSerializer({
+    value: fillForEmptyDates(values, { with: "0" }),
   }).perform();
   res.send(json);
 });
@@ -69,7 +123,7 @@ router.post("/stats/average-sessions-per-user-per-day", async (req, res) => {
   const queryResult = await models.connections.default.query(query, { type: "SELECT" });
   const values = queryResult.map((r) => ({ label: r.day, values: { value: r.average_sessions } }));
   let json = new Liana.StatSerializer({
-    value: values,
+    value: fillForEmptyDates(values, { with: "0" }),
   }).perform();
   res.send(json);
 });
@@ -96,7 +150,7 @@ router.post("/stats/average-sessions-per-active-user-per-day", async (req, res) 
   const queryResult = await models.connections.default.query(query, { type: "SELECT" });
   const values = queryResult.map((r) => ({ label: r.day, values: { value: r.average_sessions } }));
   let json = new Liana.StatSerializer({
-    value: values,
+    value: fillForEmptyDates(values, { with: "0" }),
   }).perform();
   res.send(json);
 });
