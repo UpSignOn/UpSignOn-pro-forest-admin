c63e97ee482328b8415a67de02d74826300bd86a
Gireg <gireg.dekerdanet@hotmail.fr>
2021-05-04 11:14:11 +0200

    update dependencies

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-const express = require('express');
-const requireAll = require('require-all');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const jwt = require('express-jwt');
-const morgan = require('morgan');
-const {
-  errorHandler,
-  ensureAuthenticated,
-  PUBLIC_ROUTES,
-} = require('forest-express-sequelize');
+const express = require("express");
+const requireAll = require("require-all");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("express-jwt");
+const morgan = require("morgan");
+const { errorHandler, ensureAuthenticated, PUBLIC_ROUTES } = require("forest-express-sequelize");
 
 const app = express();
 
 let allowedOrigins = [/\.forestadmin\.com$/, /localhost:\d{4}$/];
 
 if (process.env.CORS_ORIGINS) {
-  allowedOrigins = allowedOrigins.concat(process.env.CORS_ORIGINS.split(','));
+  allowedOrigins = allowedOrigins.concat(process.env.CORS_ORIGINS.split(","));
 }
 
 const corsConfig = {
   origin: allowedOrigins,
-  allowedHeaders: ['Authorization', 'X-Requested-With', 'Content-Type'],
+  allowedHeaders: ["Authorization", "X-Requested-With", "Content-Type"],
   maxAge: 86400, // NOTICE: 1 day
   credentials: true,
 };
 
-app.use(morgan('tiny'));
-app.use('/forest/authentication', cors({
-  ...corsConfig,
-  // The null origin is sent by browsers for redirected AJAX calls
-  // we need to support this in authentication routes because OIDC
-  // redirects to the callback route
-  origin: corsConfig.origin.concat('null')
-}));
+app.use(morgan("tiny"));
+app.use(
+  "/forest/authentication",
+  cors({
+    ...corsConfig,
+    // The null origin is sent by browsers for redirected AJAX calls
+    // we need to support this in authentication routes because OIDC
+    // redirects to the callback route
+    origin: corsConfig.origin.concat("null"),
+  })
+);
 app.use(cors(corsConfig));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(jwt({
-  secret: process.env.FOREST_AUTH_SECRET,
-  credentialsRequired: false,
-}));
+app.use(
+  jwt({
+    secret: process.env.FOREST_AUTH_SECRET,
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+  })
+);
 
-app.use('/forest', (request, response, next) => {
+app.use("/forest", (request, response, next) => {
   if (PUBLIC_ROUTES.includes(request.url)) {
     return next();
   }
@@ -54,13 +56,13 @@ app.use('/forest', (request, response, next) => {
 });
 
 requireAll({
-  dirname: path.join(__dirname, 'routes'),
+  dirname: path.join(__dirname, "routes"),
   recursive: true,
-  resolve: (Module) => app.use('/forest', Module),
+  resolve: (Module) => app.use("/forest", Module),
 });
 
 requireAll({
-  dirname: path.join(__dirname, 'middlewares'),
+  dirname: path.join(__dirname, "middlewares"),
   recursive: true,
   resolve: (Module) => Module(app),
 });
