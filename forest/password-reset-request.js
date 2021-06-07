f007326403a8705e426d4947d51ac708a46224f5
Gireg <gireg.dekerdanet@hotmail.fr>
2021-06-07 14:29:58 +0200

    add user email to reset password requests

--- forest/password-reset-request.js
+++ forest/password-reset-request.js
@@ -1,4 +1,5 @@
 const { collection } = require("forest-express-sequelize");
+const models = require("../models");
 
 // This file allows you to add to your Forest UI:
 // - Smart actions: https://docs.forestadmin.com/documentation/reference-guide/actions/create-and-manage-smart-actions
@@ -13,6 +14,20 @@ collection("passwordResetRequest", {
       endpoint: "/forest/actions/grant-password-reset-request",
     },
   ],
-  fields: [],
+  fields: [
+    {
+      field: "userEmail",
+      type: "String",
+      description: "",
+      isReadOnly: true,
+      get: async (resetRequest) => {
+        const userEmail = await models.connections.default.query(
+          `SELECT email FROM users AS u INNER JOIN user_devices AS ud ON ud.user_id=u.id INNER JOIN password_reset_request AS prr ON prr.device_id=ud.id WHERE prr.id=${resetRequest.id}`,
+          { type: "SELECT" }
+        );
+        return userEmail[0].email;
+      },
+    },
+  ],
   segments: [],
 });
